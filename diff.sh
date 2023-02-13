1a7673262cdaa012ab96ac5e63d0cacfdaac9d5d
reSHARMA <reshabhsh@gmail.com>
2023-02-13 07:33:42 +0000

    ignore more instruction which create noise in the diff

--- diff.sh
+++ diff.sh
@@ -9,11 +9,17 @@ clean () {
  sed  -i '/^[a-z,A-Z]/d' $1
  sed -i 's/.*\t//' $1
  sed -i '/nop/d' $1
+ sed -i '/lea/d' $1
+ sed -i '/je/d' $1
+ sed -i '/jne/d' $1
+ sed -i '/jmp/d' $1
+ sed -i '/jae/d' $1
+ sed -i '/jbe/d' $1
 }
 
 objd $1
 objd $2
 clean $1.asm
 clean $2.asm
-diff -I '0x*\s' $1.asm $2.asm > diff.txt
-python3 diff.py $1.asm diff.txt
+diff --strip-trailing-cr -B -Z -I "(0x|0X)?[a-fA-F0-9]+" $1.asm $2.asm > diff.txt
+python3 ~/diff.py $1.asm diff.txt
